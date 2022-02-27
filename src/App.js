1265dcc0954d05e89e1bdc291424db8da19ed779
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-02-27 22:29:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import exifr, { parse } from "exifr";
+import GoogleMapReact from "google-map-react";
+import Marker from "./Marker";
 
 function App() {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState();
+  const [gpsImg, setGpsImg] = useState({
+    lat: "",
+    lng: "",
+  });
+
+  async function handleChange(e) {
+    parse(fileRef.current.files[0], { gps: true }).then((data) => {
+      const { latitude, longitude } = data;
+      setGpsImg({
+        lat: latitude,
+        lng: longitude,
+      });
+    });
+  }
+
+  const parseExif = () => {
+    console.log(fileRef.current.files[0]);
+    parse(fileRef.current.files[0], { gps: true }).then((data) => {
+      const { latitude, longitude } = data;
+      console.log(latitude, longitude);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <input
+        type="file"
+        id="file"
+        accept=".jpg, .png, .heif, .heic"
+        onChange={handleChange}
+        ref={fileRef}
+        style={{
+          border: "none",
+          outlineL: "none",
+          padding: "8px 12px",
+          margin: "20px",
+        }}
+      />
+      {/* <button onClick={parseExif}>test</button> */}
+      {/* <img src={} alt="" className="img" /> */}
+      <div style={{ width: 1000, height: 600 }}>
+        <GoogleMapReact
+          defaultZoom={10}
+          defaultCenter={[10.376528, 106.343889]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Marker key={1} text={"test"} lat={gpsImg.lat} lng={gpsImg.lng} />
+        </GoogleMapReact>
+      </div>
+    </>
   );
 }
 
